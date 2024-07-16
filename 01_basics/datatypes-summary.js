58cd3768888e990c0108eee5afffdf53300aae13
shreyansh749 <shreyanshjais49@gmail.com>
2024-07-16 11:36:32 +0000

    datatype_summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,26 @@
+//1.Primitive
+/* 7 types : String , Number, Boolean, null, undefined, Symbol, BigInt
+*/
+const value=33
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 3533645636335363637n
+
+/*
+2.Reference (Non Promitive)
+  Array, Objects, Functions
+*/
+const hero = ["shaktiman","nagraaj", "dogga"];
+
+let myobj={
+    name: Shreyansh,
+    age: 19,
+}
+ const myFunction = function(){
+    console.log("hello world");
+ }
